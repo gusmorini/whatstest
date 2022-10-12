55c50a3569e03f5509de4850c2d0085d611d8d41
Gustavo Morini <gustavommh@gmail.com>
2022-10-12 12:33:57 -0300

    methods Element.prototype

--- js/controller/WhatsTestControler.js
+++ js/controller/WhatsTestControler.js
@@ -1,6 +1,6 @@
 class WhatsTest {
   constructor() {
-    console.log("WHATSTEST");
+    this.elementsPrototype();
     this.loadElements();
   }
 
@@ -13,5 +13,61 @@ class WhatsTest {
     document.querySelectorAll("[id").forEach((element) => {
       this.el[Format.getElementCase(element.id)] = element;
     });
+  }
+
+  elementsPrototype() {
+    /** oculta o elemento */
+    Element.prototype.hide = function () {
+      this.style.display = "none";
+      return this;
+    };
+    /** mostra o elemento */
+    Element.prototype.show = function () {
+      this.style.display = "block";
+      return this;
+    };
+    /** faz um toogle entre o hide e o show */
+    Element.prototype.toggle = function () {
+      this.style.display = this.style.display === "none" ? "block" : "none";
+      return this;
+    };
+    /** metodo de vários eventos
+     * recebe os eventos separados por ","
+     * ex: this.on('click, dblclick', fn)
+     */
+    Element.prototype.on = function (events, fn) {
+      events.split(",").forEach((event) => {
+        this.addEventListener(event, fn);
+      });
+      return this;
+    };
+    /** método manipular style css
+     * ex: this.css({ background: 'red', width: '50% })
+     */
+    Element.prototype.css = function (styles) {
+      for (const name in styles) {
+        this.style[name] = styles[name];
+      }
+      return this;
+    };
+    /* méotodo adicionar class */
+    Element.prototype.addClass = function (name) {
+      this.classList.add(name);
+      return this;
+    };
+    /** método remove class */
+    Element.prototype.remClass = function (name) {
+      this.classList.remove(name);
+      return this;
+    };
+    /** método toogle class */
+    Element.prototype.toggleClass = function (name) {
+      this.classList.toggle(name);
+      return this;
+    };
+    /** método verifica se a classe existe no elemento */
+    Element.prototype.hasClass = function (name) {
+      return this.classList.contains(name);
+    };
   }
 }
