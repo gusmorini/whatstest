6b7b7bb84d9e994e9bf79886ffaa276392e8eb0c
Gustavo Morini <gustavommh@gmail.com>
2022-10-12 11:53:49 -0300

    method loadElements

--- js/controller/WhatsTestControler.js
+++ js/controller/WhatsTestControler.js
@@ -1,5 +1,17 @@
 class WhatsTest {
   constructor() {
     console.log("WHATSTEST");
+    this.loadElements();
+  }
+
+  /**
+   * carrega todos elementos com id do dom
+   * e formata nomes id em camel case
+   */
+  loadElements() {
+    this.el = {};
+    document.querySelectorAll("[id").forEach((element) => {
+      this.el[Format.getElementCase(element.id)] = element;
+    });
   }
 }
--- js/util/Format.js
+++ js/util/Format.js
@@ -1,5 +1,11 @@
 class Format {
-  constructor() {
-    console.log("FORMAT CLASS");
+  /**
+   * método transforma texto em camelcase
+   * ex: meu-elemento -> meuElemento
+   */
+  static getElementCase(text) {
+    let div = document.createElement("div");
+    div.innerHTML = `<p data-${text}="banana"></p>`;
+    return Object.keys(div.firstChild.dataset)[0];
   }
 }
